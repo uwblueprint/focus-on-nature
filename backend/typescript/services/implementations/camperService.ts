e2dbe775f9d84cbd1e643a8a21fafc052f6b1b0a
rchang1163 <46568041+rchang1163@users.noreply.github.com>
2022-02-08 23:21:43 -0500

    removed firebase check

--- backend/typescript/services/implementations/camperService.ts
+++ backend/typescript/services/implementations/camperService.ts
@@ -1,5 +1,3 @@
-import * as firebaseAdmin from "firebase-admin";
-
 import ICamperService from "../interfaces/camperService";
 import MgUser, { Camper } from "../../models/camper.model";
 import { CamperDTO, UpdateCamperDTO, DropOffType } from "../../types";
@@ -8,23 +6,12 @@ import logger from "../../utilities/logger";
 
 const Logger = logger(__filename);
 
-/* Something similar necessary? */
-
-/*const getMongoUserByAuthId = async (authId: string): Promise<User> => {
-    const user: User | null = await MgUser.findOne({ authId });
-    if (!user) {
-      throw new Error(`user with authId ${authId} not found.`);
-    }
-    return user;
-  };*/
-
 class CamperService implements ICamperService {
   async updateCamperById(
     camperId: string,
     camper: UpdateCamperDTO,
   ): Promise<CamperDTO> {
     let oldCamper: Camper | null;
-    let updatedFirebaseUser: firebaseAdmin.auth.UserRecord;
 
     try {
       // must explicitly specify runValidators when updating through findByIdAndUpdate
@@ -53,47 +40,6 @@ class CamperService implements ICamperService {
       if (!oldCamper) {
         throw new Error(`camperId ${camperId} not found.`);
       }
-
-      try {
-        updatedFirebaseUser = await firebaseAdmin
-          .auth()
-          .updateUser(oldCamper.authId, { email: camper.contactEmail });
-      } catch (error) {
-        // rollback MongoDB user updates
-        try {
-          await MgUser.findByIdAndUpdate(
-            camperId,
-            {
-              firstName: camper.firstName,
-              lastName: camper.lastName,
-              age: camper.age,
-              parentName: camper.parentName,
-              contactEmail: camper.contactEmail,
-              contactNumber: camper.contactNumber,
-              //camps: camper.camps,
-              hasCamera: camper.hasCamera,
-              hasLaptop: camper.hasLaptop,
-              allergies: camper.allergies,
-              additionalDetails: camper.additionalDetails,
-              dropOffType: camper.dropOffType,
-              registrationDate: camper.registrationDate,
-              hasPaid: camper.hasPaid,
-              chargeId: camper.chargeId,
-            },
-            { runValidators: true },
-          );
-        } catch (mongoDbError: unknown) {
-          const errorMessage = [
-            "Failed to rollback MongoDB user update after Firebase user update failure. Reason =",
-            getErrorMessage(mongoDbError),
-            "MongoDB user id with possibly inconsistent data =",
-            oldCamper.id,
-          ];
-          Logger.error(errorMessage.join(" "));
-        }
-
-        throw error;
-      }
     } catch (error: unknown) {
       Logger.error(`Failed to update user. Reason = ${getErrorMessage(error)}`);
       throw error;
@@ -103,7 +49,6 @@ class CamperService implements ICamperService {
       id: camperId,
       firstName: camper.firstName,
       lastName: camper.lastName,
-      //email: updatedFirebaseUser.email ?? "",
       age: camper.age,
       parentName: camper.parentName,
       contactEmail: camper.contactEmail,
