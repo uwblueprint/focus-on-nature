9c128b1e18bd8167be0405d925afff2d6552b4be
Jason <xiongjason2002@gmail.com>
2022-02-09 21:14:34 -0800

    lint

--- backend/typescript/models/campleader.model.ts
+++ backend/typescript/models/campleader.model.ts
@@ -2,14 +2,16 @@ import { Schema, Model } from "mongoose";
 import UserSchema, { User } from "./user.model"; 
 
 export interface CampLeader extends User {
-  camps: Schema.Types.ObjectId;
+    camps: Schema.Types.ObjectId;
 }
 
-const CampLeaderModel: Model<User> = UserSchema.discriminator("CampLeader", new Schema({
-  camps: {
-    type: [{ type: Schema.Types.ObjectId, ref: "Camp" }],
-    default: [],
-  },
-}));
+const CampLeaderModel: Model<User> = UserSchema.discriminator("CampLeader",
+  new Schema({
+    camps: {
+      type: [{ type: Schema.Types.ObjectId, ref: "Camp" }],
+      default: [],
+    },
+  })
+);
 
 export default CampLeaderModel;
