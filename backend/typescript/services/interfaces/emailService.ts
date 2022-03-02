b9638241454ed25e1142718653fad63b38f0c134
Umar Yousafzai <39500143+uyousafzai54@users.noreply.github.com>
2022-03-02 15:55:31 -0500

    feat: setup email service methods (#13)
    * add custom email services
    
    * linting
    
    * Update backend/typescript/services/implementations/emailService.ts
    
    Co-authored-by: Anne Chung <40839109+annechung@users.noreply.github.com>
    
    Co-authored-by: Anne Chung <40839109+annechung@users.noreply.github.com>

--- backend/typescript/services/interfaces/emailService.ts
+++ backend/typescript/services/interfaces/emailService.ts
@@ -1,5 +1,29 @@
 interface IEmailService {
   /**
+   * Send confirmation email
+   * @param to recipient's email
+   * @throws Error if email was not sent successfully
+   */
+  sendConfirmationEmail(to: string): Promise<void>;
+  /**
+   * Send waitlist email
+   * @param to recipient's email
+   * @throws Error if email was not sent successfully
+   */
+  sendWaitlistEmail(to: string): Promise<void>;
+  /**
+   * Send waitlist admin email
+   * @param to recipient's email
+   * @throws Error if email was not sent successfully
+   */
+  sendWaitlistAdminEmail(to: string): Promise<void>;
+  /**
+   * Send cancellation email
+   * @param to recipient's email
+   * @throws Error if email was not sent successfully
+   */
+  sendCancellationEmail(to: string): Promise<void>;
+  /**
    * Send email
    * @param to recipient's email
    * @param subject email subject
