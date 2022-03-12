b3aafa761dbdc25da8c80a6b559d0a95aae2e022
Nirmal Hegde <nirmalhegde821@gmail.com>
2022-03-12 16:25:25 -0500

    fixed error logging

--- backend/typescript/rest/camperRoutes.ts
+++ backend/typescript/rest/camperRoutes.ts
@@ -79,12 +79,16 @@ camperRouter.get("/", async (req, res) => {
 camperRouter.get("/refund-confirm/:chargeId", async (req, res) => {
   const { chargeId } = req.params;
   try {
-    const camper = camperService.getCamperByChargeId(
+    const camper = await camperService.getCamperByChargeId(
       (chargeId as unknown) as number,
     );
     res.status(200).json(camper);
   } catch (error: unknown) {
-    res.status(500).json({ error: getErrorMessage(error) });
+    if (getErrorMessage(error).includes("Camper with Charge Id")) {
+      res.status(404).json({ error: getErrorMessage(error) });
+    } else {
+      res.status(500).json({ error: getErrorMessage(error) });
+    }
   }
 });
 
