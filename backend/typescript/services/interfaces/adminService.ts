53c4eef61b79cb21063b795d22687b5a0cd82100
Umar Yousafzai <umaryousafzai54@gmail.com>
2022-03-12 23:19:01 -0500

    add docs for admin service interface

--- backend/typescript/services/interfaces/adminService.ts
+++ backend/typescript/services/interfaces/adminService.ts
@@ -1,8 +1,18 @@
 import {WaiverDTO } from "../../types";
 
 interface IAdminService {
+    /**
+     * Creates a new waiver document or updates it if it already exists. 
+     * @param waiver the waver to be created or updated
+     * @returns A WaiverDTO that contains the created or updated waiver. 
+     * @throws Error if waiver creation or update fails. 
+     */
     updateWaiver(waiver: WaiverDTO): Promise<WaiverDTO>
-
+    /**
+     * Get the waiver. 
+     * @returns A WaiverDTO that contains the waiver.
+     * @throws Error if waiver retrieval fails. 
+     */
     getWaiver(): Promise<WaiverDTO>
 }
 
