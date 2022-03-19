ac534525922ac675938e69c425b2014215d710d2
Amanda Guo <amanda.guo@uwaterloo.ca>
2022-03-19 15:46:42 -0400

    resolved comments

--- backend/typescript/middlewares/validators/util.ts
+++ backend/typescript/middlewares/validators/util.ts
@@ -80,12 +80,12 @@ export const validateUserEmail = (userEmail: string): boolean => {
 };
 
 export const validateMap = (
-  formResponses: any,
+  map: any,
   keyType: Type,
   valueType: Type,
 ): boolean => {
-  const keys = Object.keys(formResponses);
-  const values = Object.values(formResponses);
+  const keys = Object.keys(map);
+  const values = Object.values(map);
   for (let i = 0; i < keys.length; i += 1) {
     if (
       !validatePrimitive(values[i], valueType) ||
