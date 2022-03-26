573d5d0613fe7db97c0a3aa337f89938fd3e156d
Anne Chung <40839109+annechung@users.noreply.github.com>
2022-03-26 16:39:55 -0400

    fix: lint error (#37)

--- backend/typescript/middlewares/validators/adminValidators.ts
+++ backend/typescript/middlewares/validators/adminValidators.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { validatePrimitive } from "./util";
 
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-/* eslint-disable-next-line import/prefer-default-export */
 const validateClause = (obj: any): boolean => {
   if (!validatePrimitive(obj.text, "string")) {
     return false;
@@ -13,12 +11,18 @@ const validateClause = (obj: any): boolean => {
   return true;
 };
 
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable-next-line import/prefer-default-export */
 export const waiverUpdateValidator = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  if (!req.body.clauses || req.body.clauses.length === 0 || !req.body.clauses.every(validateClause)) {
+  if (
+    !req.body.clauses ||
+    req.body.clauses.length === 0 ||
+    !req.body.clauses.every(validateClause)
+  ) {
     return res
       .status(400)
       .send(
