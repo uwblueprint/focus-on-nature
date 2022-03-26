b85dab278c62fb9ec5e647ecd93af8d837d4a594
Umar Yousafzai <umaryousafzai54@gmail.com>
2022-03-26 15:05:32 -0400

    update docs

--- backend/typescript/services/interfaces/emailService.ts
+++ backend/typescript/services/interfaces/emailService.ts
@@ -1,28 +1,34 @@
 interface IEmailService {
   /**
-   * Send confirmation email
-   * @param to recipient's email
+   * Send confirmation email.
    * @throws Error if email was not sent successfully
    */
   sendConfirmationEmail(to: string, registrantName: string, sessionDates: string, camperName: string, camperAge: string, registrantEmail: string, registrantPhoneNumber: string, campFees: string, earlyDropOffAndPickupFees: string, totalPayment: string, link: string): Promise<void>;
   /**
-   * Send waitlist email
-   * @param to recipient's email
+   * Send cancellation email.
    * @throws Error if email was not sent successfully
    */
    sendCancellationEmail(to: string, registrantName:string): Promise<void>;
   /**
-   * Send waitlist admin email
-   * @param to recipient's email
+   * Send registration invite email. 
    * @throws Error if email was not sent successfully
    */
    registrationInviteEmail(to: string, campName:string, waitlistName:string, sessionDates: string, link: string): Promise<void>;
   /**
-   * Send cancellation email
-   * @param to recipient's email
+   * Send special needs email.
    * @throws Error if email was not sent successfully
    */
    specialNeedsEmail(to: string, registrantName: string, sessionDates: string, camperName: string, camperAge: string, registrantEmail: string, registrantPhoneNumber: string, specialNeeds: string): Promise<void>;
+  /**
+   * Send full camp email.
+   * @throws Error if email was not sent successfully
+   */
+   fullCamp(to: string, campName:string, sessionDates: string): Promise<void>;
+  /**
+   * Send camper cancellation notice email. 
+   * @throws Error if email was not sent successfully
+   */
+   camperCancellationNoticeEmail(to: string, camperName:string, campName: string, sessionDates: string): Promise<void>;
   /**
    * Send email
    * @param to recipient's email
@@ -30,10 +36,6 @@ interface IEmailService {
    * @param htmlBody email body as html
    * @throws Error if email was not sent successfully
    */
-   fullCamp(to: string, campName:string, sessionDates: string): Promise<void>;
-
-   camperCancellationNoticeEmail(to: string, camperName:string, campName: string, sessionDates: string): Promise<void>;
-
    sendEmail(to: string, subject: string, htmlBody: string): Promise<void>;
 }
 
