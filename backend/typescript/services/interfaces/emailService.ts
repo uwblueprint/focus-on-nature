da91c7ae856c2a7b434b749322a452d504af371e
Umar Yousafzai <umar.yousafzai@carta.com>
2022-03-26 14:56:30 -0400

    rebase

--- backend/typescript/services/interfaces/emailService.ts
+++ backend/typescript/services/interfaces/emailService.ts
@@ -4,25 +4,25 @@ interface IEmailService {
    * @param to recipient's email
    * @throws Error if email was not sent successfully
    */
-  sendConfirmationEmail(to: string): Promise<void>;
+  sendConfirmationEmail(to: string, registrantName: string, sessionDates: string, camperName: string, camperAge: string, registrantEmail: string, registrantPhoneNumber: string, campFees: string, earlyDropOffAndPickupFees: string, totalPayment: string): Promise<void>;
   /**
    * Send waitlist email
    * @param to recipient's email
    * @throws Error if email was not sent successfully
    */
-  sendWaitlistEmail(to: string): Promise<void>;
+   sendCancellationEmail(to: string, registrantName:string): Promise<void>;
   /**
    * Send waitlist admin email
    * @param to recipient's email
    * @throws Error if email was not sent successfully
    */
-  sendWaitlistAdminEmail(to: string): Promise<void>;
+   registrationInviteEmail(to: string, campName:string, waitlistName:string, sessionDates: string): Promise<void>;
   /**
    * Send cancellation email
    * @param to recipient's email
    * @throws Error if email was not sent successfully
    */
-  sendCancellationEmail(to: string): Promise<void>;
+   specialNeedsEmail(to: string, registrantName: string, sessionDates: string, camperName: string, camperAge: string, registrantEmail: string, registrantPhoneNumber: string, specialNeeds: string): Promise<void>;
   /**
    * Send email
    * @param to recipient's email
@@ -30,7 +30,9 @@ interface IEmailService {
    * @param htmlBody email body as html
    * @throws Error if email was not sent successfully
    */
-  sendEmail(to: string, subject: string, htmlBody: string): Promise<void>;
+   fullCamp(to: string, campName:string, sessionDates: string): Promise<void>;
+
+   camperCancellationNoticeEmail(to: string, camperName:string, campName: string, sessionDates: string): Promise<void>;
 }
 
 export default IEmailService;
