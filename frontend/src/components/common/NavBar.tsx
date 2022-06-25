b84186f1ab83930f37155548d5e195a1c5c07d20
Ryan Chang <ryanchang0501@gmail.com>
2022-06-25 15:43:13 -0400

    changed nav bar buttons to tabs

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -8,10 +8,13 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
+  Tabs,
+  Tab,
+  TabList,
 } from "@chakra-ui/react";
 import { LockIcon, ChevronDownIcon } from "@chakra-ui/icons";
 
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { Link as ReactLink, useHistory } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCampground, faPenToSquare } from "@fortawesome/free-solid-svg-icons";
@@ -23,10 +26,10 @@ import AuthContext from "../../contexts/AuthContext";
 import { Role } from "../../types/AuthTypes";
 
 import COLOR from "../../theme/colors";
+import { FontWeights } from "../../theme/textStyles";
 
 const NavBar = (): JSX.Element => {
   const { authenticatedUser, setAuthenticatedUser } = useContext(AuthContext);
-  const [hoveredItem, setHoveredItem] = useState("");
   const history = useHistory();
 
   const onLogOutClick = async () => {
@@ -38,121 +41,111 @@ const NavBar = (): JSX.Element => {
     }
   };
 
-  useEffect(() => {
-    console.log(hoveredItem);
-  });
+  const navigate = (index: number) => {
+    switch (index) {
+      case 0:
+        history.push(Routes.CAMPS_PAGE);
+        break;
+      case 1:
+        history.push(Routes.GLOBAL_FORMS_PAGE);
+        break;
+      case 2:
+        history.push(Routes.ACCESS_CONTROL_PAGE);
+        break;
+      default:
+        history.push(Routes.CAMPS_PAGE);
+        break;
+    }
+  };
+
+  const getIndex = () => {
+    switch (window.location.pathname) {
+      case Routes.CAMPS_PAGE:
+        return 0;
+      case Routes.GLOBAL_FORMS_PAGE:
+        return 1;
+      case Routes.ACCESS_CONTROL_PAGE:
+        return 2;
+      default:
+        return 0;
+    }
+  };
 
   return (
     <Container maxWidth="100vw">
       <Flex direction="row" align="center" justify="space-between">
-        <Image src={FONIcon} alt="FON icon" display="inline"/>
-          {authenticatedUser ? (
-            <>
-              {authenticatedUser.role === Role.ADMIN ? (
-                <Stack direction="row" justify="center">
-                  <Button
-                    bg="#FFF"
-                    _hover={{
-                      color: COLOR.primary.green[100],
-                    }}
-                    _active={{
-                      color: COLOR.primary.green[100],
-                    }}
-                    _focus={{
-                      color: COLOR.primary.green[100],
-                    }}
-                    onClick={() => {
-                      setHoveredItem("Camps");
-                      history.push(Routes.CAMPS_PAGE);
-                    }}
-                    leftIcon={<FontAwesomeIcon icon={faCampground} />}
-                  >
-                    Camps
-                  </Button>
-                  <Button
-                    bg="#FFF"
-                    _hover={{
-                      color: COLOR.primary.green[100],
-                    }}
-                    _active={{
-                      color: COLOR.primary.green[100],
-                    }}
-                    _focus={{
-                      color: COLOR.primary.green[100],
-                    }}
-                    onClick={() => {
-                      setHoveredItem("Global Forms");
-                      history.push(Routes.GLOBAL_FORMS_PAGE);
-                    }}
-                    leftIcon={<FontAwesomeIcon icon={faPenToSquare} />}
-                  >
-                    Global Forms
-                  </Button>
-                  <Button
-                    bg="#FFF"
-                    _hover={{
-                      color: COLOR.primary.green[100],
-                    }}
-                    _active={{
-                      color: COLOR.primary.green[100],
-                    }}
-                    _focus={{
-                      color: COLOR.primary.green[100],
-                    }}
-                    onClick={() => {
-                      setHoveredItem("Access Control");
-                      history.push(Routes.ACCESS_CONTROL_PAGE);
-                    }}
-                    leftIcon={<LockIcon />}
-                  >
-                    Access Control
-                  </Button>
-                </Stack>
-              ) : (
-                <Button
-                  bg="#FFF"
-                  _hover={{
+        <Image src={FONIcon} alt="FON icon" display="inline" />
+        {authenticatedUser ? (
+          <>
+            <Tabs
+              variant="unstyled"
+              onChange={navigate}
+              defaultIndex={getIndex as any}
+            >
+              <TabList>
+                <Tab
+                  id="Camps"
+                  _selected={{
                     color: COLOR.primary.green[100],
+                    fontWeight: FontWeights.BOLD,
                   }}
-                  _active={{
-                    color: COLOR.primary.green[100],
-                  }}
-                  _focus={{
-                    color: COLOR.primary.green[100],
-                  }}
-                  onClick={() => {
-                    setHoveredItem("Camps");
-                    history.push(Routes.CAMPS_PAGE);
-                  }}
-                  leftIcon={<FontAwesomeIcon icon={faCampground} />}
                 >
-                  Camps
-                </Button>
-              )}
-            </>
-          ) : null}
-          {authenticatedUser ? (
-            <>
-              <Menu>
-                <MenuButton as={Button} rightIcon={<ChevronDownIcon />} bg="#FFF"
+                  <FontAwesomeIcon icon={faCampground} />
+                  &nbsp; Camps
+                </Tab>
+                {authenticatedUser.role === Role.ADMIN && (
+                  <>
+                    <Tab
+                      id="Global Forms"
+                      _selected={{
+                        color: COLOR.primary.green[100],
+                        fontWeight: FontWeights.BOLD,
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faPenToSquare} />
+                      &nbsp; Global Forms
+                    </Tab>
+                    <Tab
+                      id="Access Control"
+                      _selected={{
+                        color: COLOR.primary.green[100],
+                        fontWeight: FontWeights.BOLD,
+                      }}
+                    >
+                      <LockIcon />
+                      &nbsp; Access Control
+                    </Tab>
+                  </>
+                )}
+              </TabList>
+            </Tabs>
+          </>
+        ) : null}
+        {authenticatedUser ? (
+          <>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rightIcon={<ChevronDownIcon />}
+                bg="#FFF"
                 _hover={{
-                  color: 'none',
+                  color: "none",
                 }}
                 _active={{
-                  color: 'none',
+                  color: "none",
                 }}
                 _focus={{
-                  color: 'none',
+                  color: "none",
                 }}
-                >
-                  {authenticatedUser?.firstName} {authenticatedUser?.lastName}
-                </MenuButton>
-                <MenuList>
-                  <MenuItem onClick={onLogOutClick}>Logout</MenuItem>
-                </MenuList>
-              </Menu>
-            </>
-          ) : null}
+              >
+                {authenticatedUser?.firstName} {authenticatedUser?.lastName}
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={onLogOutClick}>Logout</MenuItem>
+              </MenuList>
+            </Menu>
+          </>
+        ) : null}
       </Flex>
     </Container>
   );
