64aa6fbef4f8514104971f0317c1aa05a8b06f7d
Anne Chung <annechung301@gmail.com>
2022-08-10 22:14:39 -0400

    Add else clause

--- backend/typescript/rest/campRoutes.ts
+++ backend/typescript/rest/campRoutes.ts
@@ -48,11 +48,10 @@ campRouter.get("/:id", async (req, res) => {
     } else if (req.query?.wId || req.query?.session) {
       const error = new Error(`missing wId or session query param`);
       res.status(401).json({ error: getErrorMessage(error) });
+    } else {
+      const camp = await campService.getCampById(req.params.id);
+      res.status(200).json(camp);
     }
-
-    const camp = await campService.getCampById(req.params.id);
-
-    res.status(200).json(camp);
   } catch (error: unknown) {
     res.status(500).json({ error: getErrorMessage(error) });
   }
