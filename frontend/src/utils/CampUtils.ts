0129203b7e123ac1f3a7a88415116a0da50f5fb7
Anne Chung <annechung301@gmail.com>
2022-08-27 23:44:33 -0400

    Fix location string

--- frontend/src/utils/CampUtils.ts
+++ frontend/src/utils/CampUtils.ts
@@ -1,10 +1,8 @@
 import type { Location } from "../types/CampsTypes";
 
-const locationToString = (location?: Location): string => {
-  if (location) {
-    return `${location.streetAddress1} ${location.streetAddress2} ${location.city} ${location.province} ${location.postalCode}`;
-  }
-  return "";
+const locationToString = (location: Location): string => {
+  return `${location.streetAddress1}, ${location.streetAddress2 ?? ""}
+    ${location.city} ${location.province}, ${location.postalCode}`;
 };
 
 export default locationToString;
