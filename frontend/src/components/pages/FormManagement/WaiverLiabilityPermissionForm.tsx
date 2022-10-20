5ee0eb1a02d05227d30ec483b78659399851d5d0
Jasraj Dhanoa <50252969+Jasraj-Dhanoa@users.noreply.github.com>
2022-10-19 21:26:59 -0400

    Feat/form management title tabs (#130)
    * Create form management page
    
    * Add styling to page and tabs

---
+++ frontend/src/components/pages/FormManagement/WaiverLiabilityPermissionForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WaiverLiabilityPermissionForm = (): React.ReactElement => {
+  return (
+    <div>
+      <h2>Waiver Liability and Permission Form Placeholder</h2>
+    </div>
+  );
+};
+
+export default WaiverLiabilityPermissionForm;
