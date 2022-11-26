963db1b3a5d7b262b1a500313d35c963236b99bc
Jasmine Bal <baljasminek@gmail.com>
2022-11-26 17:43:08 -0500

    first commit

--- frontend/src/components/pages/CampCreation/CampDetails/index.tsx
+++ frontend/src/components/pages/CampCreation/CampDetails/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Box, Text } from "@chakra-ui/react";
+import CampCreationForm from "./CampCreationForm";
 
 const CampCreationDetails = (): JSX.Element => {
   return (
     <Box>
       <Text textStyle="displayXLarge">Camp Details</Text>
+      <CampCreationForm/>
     </Box>
   );
 };
