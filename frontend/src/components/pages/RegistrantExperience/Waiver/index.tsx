63dd2ffe4d5667146d50f820f6abc051c3effcef
TW1234567 <thomaswu@uwblueprint.org>
2022-11-26 22:12:39 -0500

    add missed textStyle + margin changes + redo name and date input

--- frontend/src/components/pages/RegistrantExperience/Waiver/index.tsx
+++ frontend/src/components/pages/RegistrantExperience/Waiver/index.tsx
@@ -12,8 +12,9 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Divider,
+  HStack,
 } from "@chakra-ui/react";
-import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
 import {
   OptionalClauseResponse,
   RequiredClauseResponse,
@@ -33,39 +34,40 @@ const WaiverPage = ({
 }: WaiverPageProps): React.ReactElement => {
   return (
     <Box>
-      <VStack spacing={8} pt="1" align="stretch">
-        <Text textStyle="displayXLarge">Camp Registration</Text>
-        <Text textStyle="displayLarge">{waiverInterface.campName}</Text>
+      <VStack spacing={3} pt="1" align="stretch">
+        <Text textStyle="displayXLarge">
+          {waiverInterface.campName} Registration
+        </Text>
         <Text textStyle="displayLarge" color="primary.green.100">
           Waiver
         </Text>
-        <Text textStyle="displayMedium" fontWeight="bold">
+        <Text textStyle="buttonSemiBold" pt="6">
           In consideration of the participation of my child/children, (the
           “child or children“), in the Focus on Nature photography camp/workshop
-          and all activities associated therewith,
-        </Text>
-        <Text fontWeight="bold">
-          I, the undersigned parent/guardian of the child/children, agree to the
-          following terms and conditions:
+          and all activities associated therewith, I, the undersigned
+          parent/guardian of the child/children, agree to the following terms
+          and conditions:
         </Text>
       </VStack>
 
       <VStack spacing={5} pt="10" align="stretch">
-        <Text color="primary.green.100" fontWeight="bold">
+        <Text color="primary.green.100" textStyle="heading">
           Identity and Waiver of Liability
         </Text>
 
-        <OrderedList pl="10">
+        <OrderedList pl="10" spacing={5}>
           {waiverInterface.requiredClauses.map(
             (clause: RequiredClauseResponse, index: number) => (
-              <ListItem key={index}>{clause.text}</ListItem>
+              <ListItem textStyle="bodyRegular" key={index}>
+                {clause.text}
+              </ListItem>
             ),
           )}
         </OrderedList>
       </VStack>
 
       <VStack spacing={5} pt="10" align="stretch">
-        <Text fontWeight="bold">
+        <Text textStyle="heading">
           By clicking the checkbox below, you agree to the terms and conditions
           above.
         </Text>
@@ -75,21 +77,23 @@ const WaiverPage = ({
             waiverDispatch({ type: WaiverActions.ClICK_REQUIRED_CLAUSE })
           }
         >
-          I agree to the above identity and liability waiver conditions{" "}
-          <Text as="span" color="red">
-            *
+          <Text textStyle="bodyRegular">
+            I agree to the above identity and liability waiver conditions{" "}
+            <Text as="span" color="text.critical.100" verticalAlign="super">
+              *
+            </Text>
           </Text>
         </Checkbox>
       </VStack>
-
-      <VStack spacing={5} pt="10" align="stretch">
-        <Text color="primary.green.100" fontWeight="bold">
+      <Divider pt="4" pb="4" borderColor="border.secondary.100" />
+      <VStack spacing={5} pt="8" align="stretch">
+        <Text color="primary.green.100" textStyle="heading">
           Additional Clauses
         </Text>
         {waiverInterface.optionalClauses.map(
           (clause: OptionalClauseResponse, index: number) => (
             <VStack spacing={6} pt="2" align="stretch" key={index}>
-              <Text>{clause.text}</Text>
+              <Text textStyle="bodyRegular">{clause.text}</Text>
               <RadioGroup
                 onChange={() =>
                   waiverDispatch({
@@ -101,13 +105,13 @@ const WaiverPage = ({
                 value={String(clause.agreed)}
               >
                 <Stack spacing={5} direction="row">
-                  <Radio value="true" mb="0">
+                  <Radio value="true" mb="0" textStyle="bodyRegular">
                     {/* NOTE!?: I use mb='0' because there's a margin misalignment issue with Radio component */}
                     I agree
                   </Radio>
-                  <Radio value="false">
+                  <Radio value="false" textStyle="bodyRegular">
                     I{" "}
-                    <Text as="span" fontWeight="bold">
+                    <Text as="span" textStyle="bodyBold">
                       do not
                     </Text>{" "}
                     agree
@@ -118,16 +122,52 @@ const WaiverPage = ({
           ),
         )}
       </VStack>
+      <Divider
+        pt="4"
+        pb="4"
+        borderColor="border.secondary.100"
+        textStyle="heading"
+      />
+      <Text textStyle="heading" pt="6">
+        By typing my name below, I hereby declare that I fully release and
+        discharge Focus on Nature, its employees and volunteers from all
+        liabilities to which I have agreed to above.
+      </Text>
+      <HStack pt="4" spacing="6">
+        <FormControl width="30%">
+          <FormLabel>
+            <Text textStyle="buttonSemiBold">
+              Full Name{" "}
+              <Text
+                as="span"
+                color="text.critical.100"
+                fontSize="xs"
+                verticalAlign="super"
+              >
+                *
+              </Text>
+            </Text>
+          </FormLabel>
+          <Input textStyle="bodyRegular" fontFamily="cursive" />
+        </FormControl>
 
-      <FormControl pt="9" width="40%">
-        <FormLabel fontWeight="bold">Name</FormLabel>
-        <Input fontFamily="cursive" />
-      </FormControl>
-
-      <FormControl pt="5" width="40%">
-        <FormLabel fontWeight="bold">Date</FormLabel>
-        <Input />
-      </FormControl>
+        <FormControl width="30%">
+          <FormLabel textStyle="buttonSemiBold">
+            <Text textStyle="buttonSemiBold">
+              Date{" "}
+              <Text
+                as="span"
+                color="text.critical.100"
+                fontSize="xs"
+                verticalAlign="super"
+              >
+                *
+              </Text>
+            </Text>
+          </FormLabel>
+          <Input textStyle="bodyRegular" type="date" />
+        </FormControl>
+      </HStack>
     </Box>
   );
 };
