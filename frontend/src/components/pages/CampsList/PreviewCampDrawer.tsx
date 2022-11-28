f02fa3d6f3c62b14e721b1ac97176b5d1ca1e17c
Ivan Yu <85257356+IvanYu327@users.noreply.github.com>
2022-11-27 22:52:17 -0500

    Create camp list preview drawer (#163)

---
+++ frontend/src/components/pages/CampsList/PreviewCampDrawer.tsx
@@ -0,0 +1,149 @@
+import { Box, Button, Text, Flex, Tag } from "@chakra-ui/react";
+import React from "react";
+import { FontWeights } from "../../../theme/textStyles";
+import CampStatusLabel from "./CampStatusLabel";
+import { getCampStatus, locationString } from "../../../utils/CampUtils";
+import { CampResponse, CampSession } from "../../../types/CampsTypes";
+import PreviewModalSessionRow from "./PreviewModalSessionRow";
+
+type CampDrawerProps = {
+  isOpen: boolean;
+  onClose: () => void;
+  camp: CampResponse | undefined;
+};
+
+const PreviewCampDrawer = ({
+  isOpen,
+  onClose,
+  camp,
+}: CampDrawerProps): JSX.Element => {
+  return camp ? (
+    <Flex
+      bg="background.white.100"
+      w="500px"
+      h="calc(100vh - 68px)"
+      position="absolute"
+      pos="fixed"
+      right={isOpen ? "0px" : "-500px"}
+      transition="right 0.5s"
+      borderWidth="1px 0 1px 1px"
+      borderColor="border.secondary.100"
+      flexDirection="column"
+    >
+      <Box paddingRight="32px" paddingLeft="32px">
+        <Text
+          onClick={onClose}
+          color="#A3AEBE"
+          textDecoration="underline"
+          marginTop="32px"
+          textAlign="right"
+          textStyle="xSmallBold"
+          cursor="pointer"
+        >
+          Close
+        </Text>
+        <Text textStyle="displayMediumBold" marginTop="20px">
+          {camp.name}
+        </Text>
+
+        <Text textStyle="heading" fontWeight={FontWeights.REGULAR}>
+          {locationString(camp.location)}
+        </Text>
+
+        <Box marginTop="12px" w="fit-content">
+          {camp ? <CampStatusLabel status={getCampStatus(camp)} /> : ""}
+        </Box>
+        <Box
+          marginTop="20px"
+          padding="16px 32px"
+          borderRadius="5px"
+          borderWidth="2px"
+          borderColor="border.input.100"
+        >
+          <Text textStyle="buttonSemiBold">
+            Daily Camp Fee:{" "}
+            <Text as="span" textStyle="buttonRegular">
+              ${camp.fee} per day
+            </Text>
+          </Text>
+          <Text textStyle="buttonSemiBold">
+            Age Range:{" "}
+            <Text as="span" textStyle="buttonRegular">
+              {camp.ageLower} to {camp.ageUpper}
+            </Text>
+          </Text>
+        </Box>
+        <Box margin="20px 0">
+          <Button
+            marginRight="20px"
+            aria-label="View Camp"
+            border="1px"
+            borderRadius="5px"
+            color="primary.green.100"
+            bg="white"
+            borderColor="primary.green.100"
+            minWidth="-webkit-fit-content"
+          >
+            View Camp
+          </Button>
+          <Button
+            marginRight="20px"
+            aria-label="View Camp"
+            border="1px"
+            borderRadius="5px"
+            color="secondary.critical.100"
+            bg="white"
+            borderColor="secondary.critical.100"
+            minWidth="-webkit-fit-content"
+          >
+            Delete Camp
+          </Button>
+        </Box>
+      </Box>
+
+      <Flex
+        bg="background.grey.100"
+        paddingRight="32px"
+        paddingLeft="32px"
+        borderTop="1px"
+        borderColor="border.secondary.100"
+        overflowY="auto"
+        flexGrow="1"
+        flexDirection="column"
+        sx={{
+          "&::-webkit-scrollbar": {
+            backgroundColor: `white`,
+          },
+          "&::-webkit-scrollbar-thumb": {
+            width: "8px",
+            borderRadius: "12px",
+            backgroundColor: `#A3AEBE`,
+          },
+          "&::-webkit-scrollbar-button": {
+            display: `none`,
+          },
+        }}
+      >
+        <Text textStyle="buttonSemiBold" marginTop="24px" marginBottom="20px">
+          {camp.campSessions.length} Session
+          {camp.campSessions.length !== 1 ? "s" : ""}
+        </Text>
+
+        {camp.campSessions.map((session: CampSession, key: number) => {
+          return (
+            <PreviewModalSessionRow
+              key={key}
+              sessionNum={key + 1}
+              status={getCampStatus(camp)}
+              session={session}
+            />
+          );
+        })}
+      </Flex>
+    </Flex>
+  ) : (
+    <></>
+  );
+};
+
+export default PreviewCampDrawer;
--- frontend/src/components/pages/CampsList/index.tsx
+++ frontend/src/components/pages/CampsList/index.tsx
@@ -1,26 +1,57 @@
-import { Container } from "@chakra-ui/react";
+import { Flex, Box, useDisclosure } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { CampResponse } from "../../../types/CampsTypes";
 import CampsNavigationHeading from "./CampsNavigationHeading";
 import CampsTable from "./CampsTable";
+import PreviewCampDrawer from "./PreviewCampDrawer";
 
 const CampsListPage = (): React.ReactElement => {
   const [year, setYear] = useState(new Date().getFullYear());
+  const {
+    isOpen: isDrawerOpen,
+    onOpen: onDrawerOpen,
+    onClose: onDrawerClose,
+  } = useDisclosure();
+  const [campDrawerInfo, setCampDrawerInfo] = useState<CampResponse>();
 
   return (
-    <Container
-      maxWidth="100vw"
-      minHeight="100vh"
-      px="3em"
-      py="3em"
-      background="background.grey.200"
+    <Flex
+      width={isDrawerOpen ? "calc(100% - 500px)" : "100%"}
+      transition="width 0.5s"
+      top="68px"
     >
-      <CampsNavigationHeading
-        year={year}
-        onNavigateLeft={() => setYear(year - 1)}
-        onNavigateRight={() => setYear(year + 1)}
+      <Box
+        minHeight="100vh"
+        px="3em"
+        py="3em"
+        background="background.grey.200"
+        flex="1"
+      >
+        <CampsNavigationHeading
+          year={year}
+          onNavigateLeft={() => {
+            setYear(year - 1);
+            onDrawerClose();
+          }}
+          onNavigateRight={() => {
+            setYear(year + 1);
+            onDrawerClose();
+          }}
+        />
+        <CampsTable
+          year={year}
+          isDrawerOpen={isDrawerOpen}
+          onDrawerOpen={onDrawerOpen}
+          campDrawerInfo={campDrawerInfo}
+          setCampDrawerInfo={setCampDrawerInfo}
+        />
+      </Box>
+      <PreviewCampDrawer
+        isOpen={isDrawerOpen}
+        onClose={onDrawerClose}
+        camp={campDrawerInfo}
       />
-      <CampsTable year={year} />
-    </Container>
+    </Flex>
   );
 };
 
