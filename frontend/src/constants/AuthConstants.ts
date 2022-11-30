628a861859976a363db410a093421eaa6c2d3dbb
Amanda Misjuwar <37950626+AmandaMisjuwar@users.noreply.github.com>
2022-11-30 07:44:19 -0500

    fix: Replace BEARER_TOKEN constant to be a function (#168)

--- frontend/src/constants/AuthConstants.ts
+++ frontend/src/constants/AuthConstants.ts
@@ -2,9 +2,11 @@ import { getLocalStorageObjProperty } from "../utils/LocalStorageUtils";
 
 const AUTHENTICATED_USER_KEY = `${window.location.hostname}:AUTHENTICATED_USER`;
 
-export const BEARER_TOKEN = `Bearer ${getLocalStorageObjProperty(
-  AUTHENTICATED_USER_KEY,
-  "accessToken",
-)}`;
+export const getBearerToken = (): string => {
+  return `Bearer ${getLocalStorageObjProperty(
+    AUTHENTICATED_USER_KEY,
+    "accessToken",
+  )}`;
+};
 
 export default AUTHENTICATED_USER_KEY;
