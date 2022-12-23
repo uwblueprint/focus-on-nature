48590c30bb810ba97325284186224d29c228d23f
Jasraj Dhanoa <50252969+Jasraj-Dhanoa@users.noreply.github.com>
2022-12-23 03:11:29 -0500

    fix var name

--- frontend/src/components/pages/RegistrantExperience/SessionSelection/CampSessionBoard.tsx
+++ frontend/src/components/pages/RegistrantExperience/SessionSelection/CampSessionBoard.tsx
@@ -18,11 +18,11 @@ const CampSessionBoard = ({
     <Box>
     <Text fontSize={{ sm: 'xl', md: 'xl', lg:'3xl' }} as='b' mt={4} mb={4}>Available Camps</Text>
       <Grid templateColumns={{ sm: 'repeat(2, 1fr)', md:'repeat(3, 1fr)', lg:'repeat(3, 1fr)' }} mb={10}>
-        {camp.campSessions.map((value) => {
-          if (value.campers.length !== value.capacity) {
+        {camp.campSessions.map((campSession) => {
+          if (campSession.campers.length !== campSession.capacity) {
             return (
-              <GridItem key={value.id}>
-                  <AvailableCampCard fee={camp.fee} startTime={camp.startTime} endTime={camp.endTime} campData={value as CampSessionResponse} handleClick={handleSessionClick} disabled={sesssionTypeDisabled === 'available'}/>
+              <GridItem key={campSession.id}>
+                  <AvailableCampCard fee={camp.fee} startTime={camp.startTime} endTime={camp.endTime} campData={campSession as CampSessionResponse} handleClick={handleSessionClick} disabled={sesssionTypeDisabled === 'available'}/>
               </GridItem>
             )
           }
@@ -31,11 +31,11 @@ const CampSessionBoard = ({
       </Grid>
       <Text fontSize={{ sm: 'xl', md: 'xl', lg:'3xl' }} as='b' mt={4} mb={4}>Waitlist Only Camps</Text>
       <Grid templateColumns={{ sm: 'repeat(2, 1fr)', md:'repeat(3, 1fr)', lg:'repeat(3, 1fr)' }} mb={4}>
-        {camp.campSessions.map((value) => {
-          if (value.campers.length === value.capacity) {
+        {camp.campSessions.map((campSession) => {
+          if (campSession.campers.length === campSession.capacity) {
             return (
-              <GridItem key={value.id}>
-                <WaitlistCampCard fee={camp.fee} startTime={camp.startTime} endTime={camp.endTime} campData={value as CampSessionResponse} handleClick={handleSessionClick} disabled={sesssionTypeDisabled === 'waitlist'}/>
+              <GridItem key={campSession.id}>
+                <WaitlistCampCard fee={camp.fee} startTime={camp.startTime} endTime={camp.endTime} campData={campSession as CampSessionResponse} handleClick={handleSessionClick} disabled={sesssionTypeDisabled === 'waitlist'}/>
               </GridItem>
             )
           }
