bb3c8bfe180f79321ce97fa761e5c934f3f41f3a
Tina <thywang1@gmail.com>
2023-07-24 19:17:21 -0400

    feat: add regex check to validate email

--- frontend/src/components/pages/WaitlistExperience/PersonalInfo/personalInfoReducer.tsx
+++ frontend/src/components/pages/WaitlistExperience/PersonalInfo/personalInfoReducer.tsx
@@ -88,7 +88,8 @@ export const checkAge = (
 };
 
 export const checkEmail = (email: string): boolean => {
-  return !!email;
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  return regex.test(email);
 };
 
 export const checkPhoneNumber = (phoneNumber: string): boolean => {
